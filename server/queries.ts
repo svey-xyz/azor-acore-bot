720206dd576eb39e4949d0e5e6ee88043953fc1d
svey-xyz <x@svey.xyz>
2025-06-03 20:04:14 -0400

    username sanitization

--- server/queries.ts
+++ server/queries.ts
@@ -1,3 +1,5 @@
+import { capitalize } from "../lib/stringFunctions";
+
 export const enum QUERIES {
 	GET_CHARACTER_BY_NAME,
 }
@@ -19,9 +21,11 @@ export type expectedQueryReturnType = {
 export const queries = {
 	[QUERIES.GET_CHARACTER_BY_NAME]: ({ args }: { args: queryArgs[QUERIES.GET_CHARACTER_BY_NAME]}): string => {
 		if (!args || !args.username || typeof args.username !== 'string') {
-			throw new Error("Invalid username provided.");
+			throw new Error("Invalid username provided.");	
 		}
-		const sanitizedUsername = args.username.replace(/'/g, "''"); // Escape single quotes
+		
+		// Escape single quotes, convert to lowercase, and capitalize the first letter for consistency with database entries
+		const sanitizedUsername = capitalize(args.username.replace(/'/g, "''").toLowerCase()); 
 		return `SELECT * FROM characters WHERE name = '${sanitizedUsername}' LIMIT 1;`;
 	},
 
