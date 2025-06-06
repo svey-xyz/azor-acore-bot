7020409c425fe2784036e50b35edf55ffd4f49d2
svey-xyz <x@svey.xyz>
2025-06-05 22:38:10 -0400

    Item query

--- server/queries.ts
+++ server/queries.ts
@@ -8,12 +8,14 @@ export const enum DATABASES {
 
 export const enum QUERIES {
 	GET_CHARACTER_BY_NAME,
-	GET_ONLINE_CHARACTERS
+	GET_ONLINE_CHARACTERS,
+	GET_ITEM_BY_ENTRY
 }
 
 export const databaseMap = {
 	[QUERIES.GET_CHARACTER_BY_NAME]: DATABASES.CHARACTERS,
 	[QUERIES.GET_ONLINE_CHARACTERS]: DATABASES.CHARACTERS,
+	[QUERIES.GET_ITEM_BY_ENTRY]: DATABASES.WORLD,
 } as const
 
 export type queryArgType = {
@@ -23,11 +25,15 @@ export type queryArgType = {
 	[QUERIES.GET_ONLINE_CHARACTERS]: {
 		// No arguments needed for this query
 	},
+	[QUERIES.GET_ITEM_BY_ENTRY]: {
+		entry: number,
+	},
 }
 
 export type expectedQueryReturnType = {
 	[QUERIES.GET_CHARACTER_BY_NAME]: _character[] | null,
 	[QUERIES.GET_ONLINE_CHARACTERS]: _character[],
+	[QUERIES.GET_ITEM_BY_ENTRY]: _item[] | null,
 }
 
 export const queries = ({ _Q, args }: {_Q: QUERIES, args: queryArgType[typeof _Q]}) => {
@@ -48,6 +54,15 @@ export const queries = ({ _Q, args }: {_Q: QUERIES, args: queryArgType[typeof _Q
 
 			return `SELECT * FROM characters WHERE online = 1;`;
 
+		case QUERIES.GET_ITEM_BY_ENTRY:
+			const gibeArgs = args as queryArgType[QUERIES.GET_ITEM_BY_ENTRY];
+			if (!gibeArgs.entry || typeof gibeArgs.entry !== 'number')
+				throw new Error(`Entry as a number is a required argument for ${QUERIES.GET_ITEM_BY_ENTRY}.`);
+			return `SELECT * FROM item_template WHERE entry = ${gibeArgs.entry} LIMIT 1;`;
+
+		// Add more cases for other queries as needed
+		// You can also throw an error for unimplemented queries
+		// or return a default query if needed.
 		default:
 			throw new Error(`Query ${_Q} is not implemented.`);
 	}
